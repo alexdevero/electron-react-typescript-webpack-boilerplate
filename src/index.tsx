999b1da4563ec22c7747c4fee25dfa53bd456e5d
Alex Devero <deveroalex@gmail.com>
2019-12-08 09:06:49 +0100

    Add styles for lght & dark mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,34 @@
 import * as React from 'react'
 import { render } from 'react-dom'
+import { createGlobalStyle } from 'styled-components'
 
 import App from './components/app'
+
+const GlobalStyles = createGlobalStyle`
+  @media screen and (prefers-color-scheme: light), screen and (prefers-color-scheme: no-preference) {
+    /* Light theme */
+    body{
+      color: #000;
+      background-color: #fff;
+    }
+  }
+
+  @media screen and (prefers-color-scheme: dark) {
+    /* Dark theme */
+    body {
+      color: #fff;
+      background-color: #000;
+    }
+  }
+`
+
+const AppWrapper = () => (
+  <>
+    <GlobalStyles />
+
+    <App />
+  </>
+)
 
 // Since we are using HtmlWebpackPlugin WITHOUT a template, we should create our own root node in the body element before rendering into it
 let root = document.createElement('div')
@@ -10,4 +37,4 @@ root.id = 'root'
 document.body.appendChild(root)
 
 // Now we can render our application into it
-render(<App />, document.getElementById('root'))
+render(<AppWrapper />, document.getElementById('root'))
